4bf1b5025143a298f5d064f2ab2bdb15d207daf0
Henri Paves <henri.paves@gmail.com>
2017-06-04 11:51:22 +0300

    saab märkmeid lisada

---
+++ mysql_input.php
@@ -0,0 +1,23 @@
+<?php 
+	include 'mysql_connect.php';
+
+	$connect = mysqli_connect($host, $user, $pass, $db);
+	mysqli_query($connect, "SET CHARACTER SET UTF8") or
+		die("Error, ei saa andmebaasi charsetti seatud.");
+
+	$Note = mysqli_real_escape_string($connect, $_REQUEST['Note']);
+
+	$sql = "INSERT INTO hp_notes (note) VALUES ('$Note')";
+
+    if (mysqli_query($connect, $sql)) {
+		echo "<p>Kirje andmebaasi edukalt lisatud.</p>";
+	}
+	mysqli_close($connect);
+
+?>
+
+<script type="text/javascript">
+    window.setTimeout(function() {
+        window.location.href='index.php';
+    }, 1000);
+</script>
