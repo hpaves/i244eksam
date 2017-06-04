3a348363aeafac84bf19400babc9cd2c4dfba14e
Henri Paves <henri.paves@gmail.com>
2017-06-04 12:43:20 +0300

    kõik töötab, aga kasutaja ei lähe automaatselt kaasa

--- mysql_input.php
+++ mysql_input.php
@@ -5,9 +5,10 @@
 	mysqli_query($connect, "SET CHARACTER SET UTF8") or
 		die("Error, ei saa andmebaasi charsetti seatud.");
 
+	$User = mysqli_real_escape_string($connect, $_REQUEST['User']);
 	$Note = mysqli_real_escape_string($connect, $_REQUEST['Note']);
 
-	$sql = "INSERT INTO hp_notes (note) VALUES ('$Note')";
+	$sql = "INSERT INTO hp_notes (user, note) VALUES ('$User', '$Note')";
 
     if (mysqli_query($connect, $sql)) {
 		echo "<p>Kirje andmebaasi edukalt lisatud.</p>";
